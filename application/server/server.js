c614a778b96baa469c1a2b4ae3602254a16ca86e
Jed Graves <jgraves4@mail.sfsu.edu>
2023-03-02 02:44:12 -0800

    build front end

--- application/server/server.js
+++ application/server/server.js
@@ -2,9 +2,22 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors());
+const _dirname = path.dirname("");
+const buildPath = path.join(__dirname, "../client/build");
 
+app.use (express.static(buildPath));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html")),
+  function(err) {
+    if(err) {
+      res.status(500).send(err)
+    }
+  };
+});
 
 app.get("/", (req, res) => {
   res.send("Hello! This is the backEnd main page(endpoint).");
