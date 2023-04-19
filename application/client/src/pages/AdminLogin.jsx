684e345882e0096d591902923d270ee7f20ee942
meganlew <meganlew4@gmail.com>
2023-04-18 22:34:02 -0700

    added menu entry ui

--- application/client/src/pages/AdminLogin.jsx
+++ application/client/src/pages/AdminLogin.jsx
@@ -1,3 +1,166 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+
+import '../styles/Admin.css';
+
+
+function AdminLogin() {
+
+    const [formData, setFormData] = useState({
+        username: "",
+        password: ""
+    });
+
+    const handleChange = (e) => {
+        let obj = {
+            ...formData
+        }
+
+        obj[e.target.name] = e.target.value;
+        setFormData(obj);
+
+    };
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const res = await axios.post("/auth/adminlogin", formData);
+            if (res.data.success) {
+                window.location.href = '/admin';
+            }
+
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            const gif = document.querySelector('.matrix-gif');
+            gif.style.display = 'none';
+        }, 1500);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
+
+
+    return (
+        <div className='admin-div'
+            style={{
+                backgroundImage: 'url(https://cdn.wallpapersafari.com/89/15/0lQbAz.gif)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                height: '89vh',
+                width: '100vw',
+            }}
+        >
+
+            <img
+                className='matrix-gif'
+                src='https://cdn.wallpapersafari.com/89/15/0lQbAz.gif'
+                alt='Matrix Binary Glitch'
+            />
+
+            <div className='form-container'>
+
+                <Container component="main" maxWidth="xs">
+
+                    <Grid item xs={25} sm={16} md={20} component={Paper} elevation={6} square>
+                        <Box
+                            sx={{
+                                my: 10,
+                                mx: 10,
+                                display: 'relative',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                            }}
+                        >
+
+                            <Typography component="h1" variant="h5" fontSize={38} align='center' >
+                                Admin Sign in
+                            </Typography>
+
+                            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+
+                                <TextField
+                                    value={formData.name}
+                                    onChange={e => handleChange(e)}
+                                    autoComplete="given-name"
+                                    name="username"
+                                    label="Username"
+                                    required
+                                    fullWidth
+                                    autoFocus
+                                    InputLabelProps={{
+                                        sx: {
+                                            fontSize: '1.2rem'
+                                        }
+                                    }}
+                                    inputProps={{
+                                        style: {
+                                            fontSize: '2rem'
+                                        }
+                                    }}
+                                    sx={{ mt: 2 }}
+                                />
+                                <TextField
+                                    value={formData.name}
+                                    onChange={e => handleChange(e)}
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    InputLabelProps={{
+                                        sx: {
+                                            fontSize: '1.2rem'
+                                        }
+                                    }}
+                                    inputProps={{
+                                        style: {
+                                            fontSize: '2rem'
+                                        }
+                                    }}
+                                    sx={{ mt: 2 }}
+                                />
+
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2, fontSize: 20 }}
+                                >
+                                    Sign In
+                                </Button>
+
+                            </Box>
+                        </Box>
+                    </Grid>
+                </Container>
+
+
+
+            </div>
+        </div>
+    );
+}
+
+export default AdminLogin;
 
 
