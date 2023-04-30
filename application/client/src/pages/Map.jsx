40ba88404c6632c659ac9bcab87399aa908e925c
AbbasMahdavi021 <abbas.mahdavi021@gmail.com>
2023-04-30 04:33:33 -0700

    Moved Footer, to the Top of the page

--- application/client/src/pages/Map.jsx
+++ application/client/src/pages/Map.jsx
@@ -30,7 +30,7 @@ const apiKey = 'AIzaSyDV-F6ADu9wNi12fMyIxMMjUnnfquIV_50'
 // Map does not run into footer
 const containerStyle = {
   width: '100%',
-  height: '87vh',
+  height: '88vh',
 }
 
 // Center at SFSU
